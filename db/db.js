61fe4629b7a329479e1fc3e3f8739ceb0c7d3168
fantonix <fantonix.web@gmail.com>
2022-07-14 13:19:34 -0400

    Resolving error in adapter db/db.js line 6

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import { Low, JSONFile } from "lowdb";
 let db;
 
 async function connectToDB() {
-    const adapter = new JSONFile("./db/db.json");
+    const adapter = new JSONFile("../db/db.json");
     db = new Low(adapter);
     await db.read();
     db.data = db.data || { messages: [] };

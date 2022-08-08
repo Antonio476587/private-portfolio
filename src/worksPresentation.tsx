306569e5027fc674dbc898a6cc72760030e269ed
fantonix <fantonix.web@gmail.com>
2022-08-08 19:44:21 -0400

    Extracting content from works to works presentation

---
+++ src/worksPresentation.tsx
@@ -0,0 +1,50 @@
+export interface WorkPresentation {
+    h1: string;
+    p: string;
+    urlId: string;
+}
+
+const worksPresentation: WorkPresentation[] = [
+    {
+        h1: "My first SPA",
+        p: "With MERN stack, google sign in, JWT, etc.",
+        urlId: "work/1",
+    },
+    {
+        h1: "NFTs",
+        p: "NFTs where appear Elon Musk, Lebron James, BonsAI...",
+        urlId: "work/2",
+    },
+    {
+        h1: "Three on a line",
+        p: "How I did the little AI",
+        urlId: "work/3",
+    },
+    {
+        h1: "Three on a line game",
+        p: "You can play alone with a little AI",
+        urlId: "tic-tac-toe.html",
+    },
+    {
+        h1: "BarbaJS with CSS plugin and Gsap",
+        p: "Beatifuld and smooth transitions.",
+        urlId: "work/4",
+    },
+    {
+        h1: "My first static page.",
+        p: "This is my BonsAI page",
+        urlId: "work/5",
+    },
+    {
+        h1: "Background Complement",
+        p: "Background Complement with an interface to manage",
+        urlId: "bgcomplement.html",
+    },
+    {
+        h1: "My friends",
+        p: "They are the best choice and achieve I have made",
+        urlId: "work/6",
+    },
+];
+
+export default worksPresentation;

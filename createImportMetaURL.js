4d2b20e0296ca21b8b30903d9c183e96a637c904
fantonix <fantonix.web@gmail.com>
2022-08-15 12:51:26 -0400

    Adding fallback to support when the .env file doesn't exist

--- createImportMetaURL.js
+++ createImportMetaURL.js
@@ -1,31 +1,41 @@
 import fs from "fs";
+let envFileBuffer = null;
 
-const envFileBuffer = fs.readFileSync(".env");
+try {
+    envFileBuffer = fs.readFileSync(".env");
 
-if (envFileBuffer) {
-    const envFile = new TextDecoder().decode(envFileBuffer);
+    if (envFileBuffer) {
+        const envFile = new TextDecoder().decode(envFileBuffer);
 
-    const hasImportMetaURL = envFile.match(/import_meta_url_root[=]?([/-_-.\w]*)+/g);
+        const hasImportMetaURL = envFile.match(/import_meta_url_root[=]?([/-_-.\w]*)+/g);
 
-    if (hasImportMetaURL) {
-        const newEnvFile = envFile.replace(/import_meta_url_root[=]?([/-_-.\w]*)+/g, `import_meta_url_root=${import.meta.url}`);
+        if (hasImportMetaURL) {
+            const newEnvFile = envFile.replace(/import_meta_url_root[=]?([/-_-.\w]*)+/g, `import_meta_url_root=${import.meta.url}`);
 
-        const importMetaURLContent = hasImportMetaURL[0].match(/[=](\w+[/-_-.\w]*)/);
-        if (importMetaURLContent && importMetaURLContent !== import.meta.url) {
-            fs.writeFileSync(".env", newEnvFile);
-        } else if (importMetaURLContent === import.meta.url) {
-            null;
-        } else {
+            const importMetaURLContent = hasImportMetaURL[0].match(/[=](\w+[/-_-.\w]*)/);
+            if (importMetaURLContent && importMetaURLContent !== import.meta.url) {
+                fs.writeFileSync(".env", newEnvFile);
+            } else if (importMetaURLContent === import.meta.url) {
+                null;
+            } else {
+                fs.writeFileSync(".env", newEnvFile);
+            }
+        }
+        else {
+            const newEnvFile = envFile + `\rimport_meta_url_root=${import.meta.url}`;
+
             fs.writeFileSync(".env", newEnvFile);
         }
-    }
-    else {
-        const newEnvFile = envFile + `\rimport_meta_url_root=${import.meta.url}`;
+    } else {
+        const newImportMetaURL = `import_meta_url_root=${import.meta.url}`;
 
-        fs.writeFileSync(".env", newEnvFile);
+        fs.writeFileSync(".env", newImportMetaURL);
     }
-} else {
-    const newImportMetaURL = `import_meta_url_root=${import.meta.url}`;
+} catch (err) {
+    if (err.toString().match(/no such file or directory/ig)[0].toLowerCase() == "no such file or directory") {
+        const newImportMetaURL = `import_meta_url_root=${import.meta.url}`;
 
-    fs.writeFileSync(".env", newImportMetaURL);
+        fs.writeFileSync(".env", newImportMetaURL);
+    };
+    console.log(err);
 }

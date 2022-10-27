8c09162f5318acdcc8666385cc72389c7d25df07
fantonix <fantonix.web@gmail.com>
2022-10-27 15:10:56 -0400

    New scripts.test.js for the pug templates

---
+++ tests/templates_tests/scripts/scripts.test.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment jsdom
+ */
+import pug from "pug";
+import path from "path";
+import { __dirname as __root_dirname } from "../../../pathEMS";
+
+let compilerFunction = null;
+
+beforeAll(() => {
+    compilerFunction = pug.compileFile(path.resolve(__root_dirname, "templates/scripts/scripts.pug"), { pretty: "\t" });
+    
+    document.body.innerHTML = compilerFunction({});
+});
+
+afterAll(() => {
+    compilerFunction = null;
+
+    document.body.innerHTML = " ";
+});
+
+it("should have the same structure as in the innerHTML", () => {
+    expect(document.body.innerHTML).toEqual(compilerFunction({}));
+});
+
+describe("it should include these scripts and their corresponding attributes", () => {
+
+    test("polyfill script", () => {
+        const polyfillScriptHTMLElement = document.querySelector("script[src=\"/js/polyfill.bundle.min.js\"]");
+
+        expect(polyfillScriptHTMLElement).not.toBeNull();
+        expect(polyfillScriptHTMLElement.getAttribute("type")).toEqual("application/javascript");
+    });
+
+    test("app script", () => {
+        const appScriptHTMLElement = document.querySelector("script[src=\"/js/app.bundle.min.js\"]");
+
+        expect(appScriptHTMLElement).not.toBeNull();
+        expect(appScriptHTMLElement.getAttribute("type")).toEqual("application/javascript");
+    });
+
+    test("vendor script", () => {
+        const vendorScriptHTMLElement = document.querySelector("script[src=\"/js/vendor.bundle.min.js\"]");
+
+        expect(vendorScriptHTMLElement).not.toBeNull();
+        expect(vendorScriptHTMLElement.getAttribute("type")).toEqual("application/javascript");
+    });
+
+});

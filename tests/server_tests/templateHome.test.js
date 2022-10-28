0b78dcae5a13fa1f70a173328f110471deed52ea
fantonix <fantonix.web@gmail.com>
2022-10-28 14:09:30 -0400

    new gsapScriptsHTMLTemplate which is inserted in templateHome.js, templateHome.js is implementing the pug templates and the templateCompiler util function, the templateHome.test.js has been adapted to test the new structure

--- tests/server_tests/templateHome.test.js
+++ tests/server_tests/templateHome.test.js
@@ -2,6 +2,10 @@
  * @jest-environment jsdom
  */
 import templateHome from "../../server/templateHome";
+import templateCompiler, { convertToValidPugInsertion } from "../../utils/templateCompiler";
+
+import gsapScripts from "../../templates/head/gsapScriptsHTMLTemplate";
+
 
 test("Inside of the div#page shouldn't render nothing", () => {
     const newChildren = templateHome("");
@@ -15,4 +19,34 @@ test("Inside of the div#page shouldn render 'nothing'", () => {
     document.querySelector("html").innerHTML = newChildren;
 
     expect(document.querySelector("div#page").innerHTML.trim()).toEqual("nothing");
+});
+
+it("should content 'templates/head/head.pug' template inside of the template", () => {
+    const newChildren = templateHome("");
+
+    expect(newChildren.includes(templateCompiler("templates/head/head.pug", { headTags: convertToValidPugInsertion(gsapScripts) }))).toBeTruthy();
+});
+
+it("should content 'templates/elements/preload.pug' template inside of the template", () => {
+    const newChildren = templateHome("");
+
+    expect(newChildren.includes(templateCompiler("templates/elements/preload.pug"))).toBeTruthy();
+});
+
+it("should content 'templates/elements/home.pug' template inside of the template", () => {
+    const newChildren = templateHome("");
+
+    expect(newChildren.includes(templateCompiler("templates/elements/home.pug"))).toBeTruthy();
+});
+
+it("should content 'templates/elements/nav.pug' template inside of the template", () => {
+    const newChildren = templateHome("");
+
+    expect(newChildren.includes(templateCompiler("templates/elements/nav.pug"))).toBeTruthy();
+});
+
+it("should content 'templates/scripts/scripts.pug' template inside of the template", () => {
+    const newChildren = templateHome("");
+
+    expect(newChildren.includes(templateCompiler("templates/scripts/scripts.pug"))).toBeTruthy();
 });

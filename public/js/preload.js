883fe52a5c3aca7b5b4227324bcff357e0a7c0a9
fantonix <fantonix.web@gmail.com>
2022-11-14 15:37:00 -0400

    Refactoring the preload to make it more professional and beatiful

--- public/js/preload.js
+++ public/js/preload.js
@@ -4,18 +4,17 @@ const preload = document.querySelector(".preload");
 
 const windowLocation = window.location;
 let animation1;
-let animation2;
 
 let windowLoaded = false;
 
-const colors = [
-    "rgb(0, 94, 94)",
-    "rgb(33, 34, 34))",
-    "rgb(38, 0, 88)",
-    "rgb(77, 3, 52)",
-    "rgb(2, 53, 19)",
-    "rgb(68, 36, 0)",
-];
+// const colors = [
+//     "rgb(0, 94, 94)",
+//     "rgb(33, 34, 34))",
+//     "rgb(38, 0, 88)",
+//     "rgb(77, 3, 52)",
+//     "rgb(2, 53, 19)",
+//     "rgb(68, 36, 0)",
+// ];
 
 // Shortcut functions
 const animateConWelcome = () => document.querySelector(".con-welcome").classList.add("animate__animated", "animate__fadeInDown", "animate__slow");
@@ -42,15 +41,15 @@ window.addEventListener("load", () => {
     else window.scrollMaxY = window.scrollY;
 }, { once: true });
 
-function randomValues() {
-    if (windowLoaded) return;
-    gsap.to(preload, {
-        backgroundColor: gsap.utils.random(colors),
-        duration: 3,
-        power: "power1",
-        onComplete: randomValues,
-    });
-}
+// function randomValues() {
+//     if (windowLoaded) return;
+//     gsap.to(preload, {
+//         backgroundColor: gsap.utils.random(colors),
+//         duration: 3,
+//         power: "power1",
+//         onComplete: randomValues,
+//     });
+// }
 
 function handleAnimationEnd(event) {
     event.stopPropagation();
@@ -73,28 +72,15 @@ function preloadFinished() {
 
 function preloadCharged() {
     if (!windowLoaded) {
+
         window.addEventListener("load", () => {
             scrollToTheBeginOfTheElement();
-            paths.forEach((path, i) => {
-                animation2 = gsap.to(path, {
-                    strokeDashoffset: gsap.getProperty(path, "stroke-dasharray") + 0.5,
-                    duration: 2.5,
-                    ease: "power2",
-                    onComplete: preloadFinished,
-                    onCompleteParams: [animation2],
-                });
-            });
+            preloadFinished();
         }, { once: true });
+
     } else {
-        paths.forEach((path, i) => {
-            animation2 = gsap.to(path, {
-                strokeDashoffset: gsap.getProperty(path, "stroke-dasharray") + 0.5,
-                duration: 2.5,
-                ease: "power2",
-                onComplete: preloadFinished,
-                onCompleteParams: [animation2],
-            });
-        });
+        scrollToTheBeginOfTheElement();
+        preloadFinished();
     }
 }
 
@@ -106,20 +92,33 @@ if (windowLocation.pathname === "/") {
             animateConWelcome();
         }, { once: true });
     } else {
-        animation1 = anime({
-            targets: paths,
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: "easeInOutSine",
-            duration: 2500,
-            delay: function (el, i) {
-                return i * 300;
-            },
-            direction: "alternate",
-            loop: false,
+        gsap.to(document.getElementById("Capa_1"), {
+            width: "5rem",
+            height: "5rem",
+            delay: 1.5,
+            duration: 1,
         });
 
-        randomValues();
+        gsap.to(document.querySelector(".quote"), {
+            opacity: 1,
+            scale: 1,
+            delay: 2.15,
+            duration: 1.5,
+            onComplete: preloadCharged,
+        });
+        
+        animation1 = anime({
+            targets: document.getElementById("Capa_1"),
+            width: "5rem",
+            height: "5rem",
+            easing: "easeInOutSine",
+            duration: 1000,
+            delay: 1500,
+            direction: "normal",
+        });
 
-        animation1.finished.then(preloadCharged);
+        // randomValues();
+
+        // animation1.finished.then(preloadCharged);
     }
 }

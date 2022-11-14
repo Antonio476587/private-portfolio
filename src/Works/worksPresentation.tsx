867cbab8e8994bac6c931a898a1c18c637eb2465
fantonix <fantonix.web@gmail.com>
2022-11-14 14:07:42 -0400

    Updating the variable urlId to workUrlId in worksPresentation.tsx

--- src/Works/worksPresentation.tsx
+++ src/Works/worksPresentation.tsx
@@ -4,7 +4,7 @@ export interface worksPresentationObject {
     img: string;
     squaredImg:string;
     alternativeText: string;
-    urlId: string;
+    workUrlId: string;
 }
 
 const worksPresentation: worksPresentationObject[] = [
@@ -12,49 +12,49 @@ const worksPresentation: worksPresentationObject[] = [
         img: "/img/SPA.webp",
         squaredImg: "/img/SPA-squared.webp",
         alternativeText: "Single Page Applicaation with MERN stack, google sign in, JWT, etc.",
-        urlId: "work/1",
+        workUrlId: "work/1",
     },
     {
         img: "/img/NFTs.webp",
         squaredImg: "/img/NFTs-squared.webp",
         alternativeText: "NFTs where appear Elon Musk, Lebron James, BonsAI...",
-        urlId: "work/2",
+        workUrlId: "work/2",
     },
     {
         img: "/img/BarbaJS.webp",
         squaredImg: "/img/BarbaJS-squared.webp",
         alternativeText: "Beatifuld and smooth transitions.",
-        urlId: "work/4",
+        workUrlId: "work/4",
     },
     {
         img: "/img/Bonsai.webp",
         squaredImg: "/img/Bonsai-squared.webp",
         alternativeText: "This is my BonsAI page",
-        urlId: "work/5",
+        workUrlId: "work/5",
     },
     {
         img: "/img/Friends.webp",
         squaredImg: "/img/Friends-squared.webp",
         alternativeText: "They are the best choice and achieve I have made",
-        urlId: "work/6",
+        workUrlId: "work/6",
     },
     {
         img: "/img/Games.webp",
         squaredImg: "/img/Games-squared.webp",
         alternativeText: "These are the games I've made, what are you waiting for watch them?",
-        urlId: "/",
+        workUrlId: "/",
     },
     {
         img: "/img/Blog.webp",
         squaredImg: "/img/Blog-squared.webp",
         alternativeText: "This is my blog, I should make it interesting",
-        urlId: "/",
+        workUrlId: "/",
     },
     {
         img: "/img/Projects.webp",
         squaredImg: "/img/Projects-squared.webp",
         alternativeText: "There are projects, but projects are ideas for more projects, so let's make more projects",
-        urlId: "/",
+        workUrlId: "/",
     },
 ];
 
@@ -62,7 +62,7 @@ const NA: worksPresentationObject = {
     img: "/img/NA.webp",
     squaredImg: "/img/NA-squared.webp",
     alternativeText: "It's a building's window, there's no work there already",
-    urlId: "/",
+    workUrlId: "/",
 };
 
 function getWorksForWall(wallID: string): worksPresentationObject[] | undefined {

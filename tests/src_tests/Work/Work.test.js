d83d4dec4a58bf7053dd3f1ec9abaa8051e61e82
fantonix <fantonix.web@gmail.com>
2022-11-24 21:32:05 -0400

    Adapting the Work.test.js to the new component

--- tests/src_tests/Work/Work.test.js
+++ tests/src_tests/Work/Work.test.js
@@ -11,11 +11,69 @@ import { useParams } from "react-router-dom";
 
 import WorkWrapper, { Work } from "../../../src/Work";
 
+jest.mock("../../../src/Work/LikeRunes", () => {
+    return function RubberLikeRunes() {
+        return (
+            <div id="like-runes">
+                I love you Nicky Minaj
+            </div>
+        );
+    };
+});
+
 jest.mock("../../../src/Work/HeaderWork", () => {
     return function RubberHeaderWork() {
         return (
-            <div>
+            <div id="header-work">
                 ta to saldo
+            </div>
+        );
+    };
+});
+
+jest.mock("../../../src/Work/CaseStudy", () => {
+    return function RubberCaseStudy({ work, changeSectionFunctions }) {
+        return (
+            <>
+                <div id="case-study">
+                    Waos
+                </div>
+                <button id="prev" onClick={changeSectionFunctions[0]}>Prev</button>
+                <button id="next" onClick={changeSectionFunctions[1]}>Next</button>
+            </>
+        );
+    };
+});
+
+jest.mock("../../../src/Work/Classic", () => {
+    return function RubberClassic({ work, changeSectionFunctions }) {
+        return (
+            <>
+                <div id="classic">
+                    Like a mojito
+                </div>
+                <button id="prev" onClick={changeSectionFunctions[0]}>Prev</button>
+                <button id="next" onClick={changeSectionFunctions[1]}>Next</button>
+            </>
+        );
+    };
+});
+
+jest.mock("../../../src/Work/Gallery", () => {
+    return function RubberGallery() {
+        return (
+            <div id="gallery">
+                What is life?
+            </div>
+        );
+    };
+});
+
+jest.mock("../../../src/Errors/NotFound", () => {
+    return function RubberNotFound() {
+        return (
+            <div id="not-found">
+                Los errores son solo errores si los cometes mas de una vez
             </div>
         );
     };
@@ -46,6 +104,57 @@ jest.mock("../../../src/Work/workContents", () => {
                 "Yo tengo hermanos, no tengo amigos",
             ],
             styles: [],
+            type: "casestudy",
+        },
+        {
+            h2: "Tirale al rubio si es pa negociar",
+            p1: "You will never know someone well enough to criticize them.",
+            media1: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            media2: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            p2: "You will never know someone well enough to criticize them.",
+            p3: "You will never know someone well enough to criticize them.",
+            media3: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            p4: "You will never know someone well enough to criticize them.",
+            media4: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            styles: ["#AAAAAA", "#BBBBBB", "#CCCCCC"],
+            type: "classic"
+        },
+        {
+            h2: "Tirale al rubio si es pa negociar",
+            p1: "You will never know someone well enough to criticize them.",
+            media1: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            media2: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            p2: "You will never know someone well enough to criticize them.",
+            p3: "You will never know someone well enough to criticize them.",
+            media3: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            p4: "You will never know someone well enough to criticize them.",
+            media4: [
+                "/imag/inate.jpg",
+                "Yo tengo hermanos, no tengo amigos",
+            ],
+            styles: ["#AAAAAA", "#BBBBBB", "#CCCCCC"],
+            type: "gallery"
         },
         {
             h2: "Tirale al rubio si es pa negociar",
@@ -71,30 +180,7 @@ jest.mock("../../../src/Work/workContents", () => {
             ],
             styles: ["#AAAAAA", "#BBBBBB", "#CCCCCC"],
         },
-        {
-            h2: "Tirale al rubio si es pa negociar",
-            p1: "You will never know someone well enough to criticize them.",
-            media1: [
-                "/imag/inate.jpg",
-                "Yo tengo hermanos, no tengo amigos",
-            ],
-            media2: [
-                "/imag/inate.jpg",
-                "Yo tengo hermanos, no tengo amigos",
-            ],
-            p2: "You will never know someone well enough to criticize them.",
-            p3: "You will never know someone well enough to criticize them.",
-            media3: [
-                "/imag/inate.jpg",
-                "Yo tengo hermanos, no tengo amigos",
-            ],
-            p4: "You will never know someone well enough to criticize them.",
-            media4: [
-                "/imag/inate.jpg",
-                "Yo tengo hermanos, no tengo amigos",
-            ],
-            styles: ["#AAAAAA", "#BBBBBB", "#CCCCCC"],
-        },
+        undefined
     ];
     return dummyWorkContents;
 });
@@ -132,7 +218,7 @@ describe("Does the WorkWrapper work well?", () => {
             root.render(<WorkWrapper />);
         });
 
-        expect(document.querySelector(".not-found h2").textContent).toEqual("Hmmm... Page not found");
+        expect(document.getElementById("not-found")).not.toBeNull();
 
         act(() => {
             const urlParams = "id";
@@ -140,7 +226,7 @@ describe("Does the WorkWrapper work well?", () => {
             root.render(<WorkWrapper />);
         });
 
-        expect(document.querySelector(".not-found h2").textContent).toEqual("Hmmm... Page not found");
+        expect(document.getElementById("not-found")).not.toBeNull();
 
         act(() => {
             const urlParams = { id: "10" };
@@ -148,60 +234,22 @@ describe("Does the WorkWrapper work well?", () => {
             root.render(<WorkWrapper />);
         });
 
-        expect(document.querySelector(".not-found h2").textContent).toEqual("Hmmm... Page not found");
+        expect(document.getElementById("not-found")).not.toBeNull();
     });
 
-    it("should render the component", () => {
+    it("should render the Work component", () => {
         act(() => {
             const urlParams = { id: "2" };
             useParams.mockReturnValue(urlParams);
             root.render(<WorkWrapper />);
         });
 
-        expect(document.querySelector("h2[title='work-title']").textContent).toEqual("Tirale al rubio si es pa negociar");
+        expect(document.getElementById("body-work")).not.toBeNull();
     });
 
 });
 
 describe("It's time to see whether the Work component works", () => {
-
-    it("should be have the dummyWorkContents' object content", () => {
-        act(() => {
-            const urlParams = { id: "2" };
-            useParams.mockReturnValue(urlParams);
-            root.render(<WorkWrapper />);
-        });
-
-        expect(document.querySelector("#body-work").getAttribute("style")).toEqual("--color-body-background: #AAAAAA; --color-foreground: #BBBBBB; --color-foreground-2: #CCCCCC;");
-        expect(document.querySelector("h2[title='work-title']").textContent).toEqual("Tirale al rubio si es pa negociar");
-        document.querySelectorAll("p").forEach((elem) => {
-            expect(elem.textContent).toEqual("You will never know someone well enough to criticize them.");
-        });
-        document.querySelectorAll("img").forEach((elem) => {
-            expect(elem.src).toEqual("http://localhost/imag/inate.jpg");
-            expect(elem.alt).toEqual("Yo tengo hermanos, no tengo amigos");
-        });
-
-    });
-
-
-    it("should render two videos and two images", () => {
-        act(() => {
-            const urlParams = { id: "1" };
-            useParams.mockReturnValue(urlParams);
-            root.render(<WorkWrapper />);
-        });
-
-        expect(document.querySelectorAll("img").length).toEqual(2);
-        expect(document.querySelectorAll("video").length).toEqual(2);
-
-        expect(document.querySelectorAll("img")[0].src).toEqual("http://localhost/imag/inate.mp4");
-        expect(document.querySelectorAll("img")[1].src).toEqual("http://localhost/imag/inate.jpg");
-        document.querySelectorAll("video").forEach((elem) => {
-            expect(elem.src).toEqual("http://localhost/imag/inate.mp4");
-        });
-
-    });
 
     it("should have the default styles", () => {
         act(() => {
@@ -214,80 +262,9 @@ describe("It's time to see whether the Work component works", () => {
 
     });
 
-    describe("Buttons functionality", () => {
-
-        it("should have the aria-label equal to Next Section or Previous Section", () => {
-            const onClickWrapper = jest.fn();
-            act(() => {
-                const urlParams = { id: "2" };
-                useParams.mockReturnValue(urlParams);
-                root.render(
-                    <button onClick={onClickWrapper}>
-                        <WorkWrapper />
-                    </button>
-                );
-            });
-
-            act(() => {
-                document.querySelector("button[aria-label='Next Section']").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            });
-
-            expect(onClickWrapper.mock.lastCall[0].target.classList.toString()).toEqual("main-footer-span");
-            expect(onClickWrapper.mock.lastCall[0].target.getAttribute("aria-label")).toEqual("Next Section");
-            expect(onClickWrapper).toHaveBeenCalledTimes(1);
-
-            act(() => {
-                document.querySelector("button[aria-label='Previous Section']").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            });
-
-            expect(onClickWrapper.mock.lastCall[0].target.classList.toString()).toEqual("main-footer-span");
-            expect(onClickWrapper.mock.lastCall[0].target.getAttribute("aria-label")).toEqual("Previous Section");
-            expect(onClickWrapper).toHaveBeenCalledTimes(2);
-        });
-
-        it("should call location assign when a button it's pressed", () => {
-            act(() => {
-                const urlParams = { id: "2" };
-                useParams.mockReturnValue(urlParams);
-                root.render(<WorkWrapper />);
-            });
-
-            act(() => {
-                document.querySelector("button[aria-label='Next Section']").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            });
-
-            expect(location.assign).toHaveBeenCalledWith("3");
-            expect(location.assign).toHaveBeenCalledTimes(1);
-            
-            act(() => {
-                document.querySelector("button[aria-label='Previous Section']").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            });
-            
-            expect(location.assign).toHaveBeenCalledWith("1");
-            expect(location.assign).toHaveBeenCalledTimes(2);
-
-        });
-
-        it("should return without calling location.assign", () => {
-            act(() => {
-                const urlParams = { id: "1" };
-                useParams.mockReturnValue(urlParams);
-                root.render(<WorkWrapper />);
-            });
-
-            act(() => {
-                document.querySelector("button[aria-label='Previous Section']").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-            });
-
-            expect(location.assign).not.toHaveBeenCalled();
-            
-        });
-
-    });
-
     it("should render the first component", () => {
         act(() => {
-            const urlParams = { id: "4" };
+            const urlParams = { id: "6" };
             useParams.mockReturnValue(urlParams);
             root.render(<WorkWrapper />);
         });
@@ -304,6 +281,131 @@ describe("It's time to see whether the Work component works", () => {
 
         expect(document.getElementById("body-work")).toBeNull();
         expect(document.querySelector(".not-found")).toBeDefined();
+    });
+
+    test("When work is undefined, it will render NotFound", () => {
+        act(() => {
+            const urlParams = { id: "5" };
+            useParams.mockReturnValue(urlParams);
+            root.render(<WorkWrapper />);
+        });
+
+        expect(document.getElementById("body-work")).toBeNull();
+        expect(document.querySelector(".not-found")).toBeDefined();        
+    });
+
+    describe("Either a type property is provided or not, it should return a selected type of presentation (template | component) or an error", () => {
+
+        test("When type is equal to 'casestudy'", () => {
+            act(() => {
+                const urlParams = { id: "1" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(document.getElementById("case-study")).not.toBeNull();
+        });
+
+        test("When type is equal to 'classic'", () => {
+            act(() => {
+                const urlParams = { id: "2" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(document.getElementById("classic")).not.toBeNull();
+        });
+
+        test("When type is equal to 'gallery'", () => {
+            act(() => {
+                const urlParams = { id: "3" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(document.getElementById("gallery")).not.toBeNull();
+        });
+
+        test("When type is not defined, it should show an error, therefore there should not be any rendered elements", () => {
+            act(() => {
+                const urlParams = { id: "4" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(document.getElementById("case-study")).toBeNull();
+            expect(document.getElementById("classic")).toBeNull();
+            expect(document.getElementById("gallery")).toBeNull();
+            expect(document.querySelector("h1").textContent).toEqual("There's no type of work to select a template. Work ID = 4");
+        });
+
+    });
+
+    describe("prevSection and nextSection functions", () => {
+
+        test("prevSection function", () => {
+            act(() => {
+                const urlParams = { id: "1" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(location.assign).not.toHaveBeenCalled();
+
+            act(() => {
+                document.querySelector("button#prev").dispatchEvent(new MouseEvent("click", { bubbles: true }));
+            });
+
+            expect(location.assign).not.toHaveBeenCalled();
+
+            act(() => {
+                const urlParams = { id: "2" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(location.assign).not.toHaveBeenCalled();
+
+            act(() => {
+                document.querySelector("button#prev").dispatchEvent(new MouseEvent("click", { bubbles: true }));
+            });
+
+            expect(location.assign).toHaveBeenCalled();
+            expect(location.assign).toHaveBeenCalledWith("1");
+        });
+
+        test("nextSection function", () => {
+            act(() => {
+                const urlParams = { id: "1" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(location.assign).not.toHaveBeenCalled();
+
+            act(() => {
+                document.querySelector("button#next").dispatchEvent(new MouseEvent("click", { bubbles: true }));
+            });
+
+            expect(location.assign).toHaveBeenCalled();
+            expect(location.assign).toHaveBeenCalledWith("2");
+
+            act(() => {
+                const urlParams = { id: "2" };
+                useParams.mockReturnValue(urlParams);
+                root.render(<WorkWrapper />);
+            });
+
+            expect(location.assign).toHaveBeenCalled();
+
+            act(() => {
+                document.querySelector("button#next").dispatchEvent(new MouseEvent("click", { bubbles: true }));
+            });
+
+            expect(location.assign).toHaveBeenCalledTimes(2);
+            expect(location.assign).toHaveBeenCalledWith("3");
+        });
+
     });
 
 });
